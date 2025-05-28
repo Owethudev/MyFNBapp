35253900be68150ada7bab5727b3ff75e1325623
Owethudev <jezileowethu@gmail.com>
2025-05-28 10:28:28 +0200

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -3,21 +3,18 @@ function loginBtn(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-
     let username1 = "owethu",password1 = "owethulogin";
     let username2 = "jazzjuune",password2 = "jazzjuunelogin";
     let username3 = "wooks",password3 = "wookslogin";
 
-    if(username == username1 && password == password1){
-        alert("welcome Owethu");
-    }
-    else if (username == username2 && password == password2){
+   if(username == username1 && password == password1){
+        alert("welcome owethu");
+   }else if(username == username2 && password == password2){
         alert("welcome jazzjuune");
-    }
-    else if(username == username3 && password == password3){
+   }else if(username == username3 && password == password3){
         alert("welcome wooks");
-    }
-    else{
-        alert("wrong details")
-    }
+   }else{
+        alert("wrong details");
+   }
+
 }
