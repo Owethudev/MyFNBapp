c956fa5abdfcf153e206c7c2e770086db4240915
Owethudev <jezileowethu@gmail.com>
2025-06-08 22:26:26 +0200

    removed the size option pop up in the store and added an option of choosing how many items of each product you would like to purchase

--- myjs.js
+++ myjs.js
@@ -18,3 +18,119 @@ function loginBtn(){
    }
 
 }
+
+
+//event handler for increase or decrease in number of items
+let currentValueGloves;
+function increaseGloves(){
+
+     currentValueGloves = parseInt(document.getElementById("howManyItemsGloves").value);
+
+     if(currentValueGloves >=0){
+     document.getElementById("howManyItemsGloves").value = currentValueGloves + 1;
+ }
+}
+
+function decreaseGloves(){
+
+     currentValueGloves = parseInt(document.getElementById("howManyItemsGloves").value);
+
+     if(currentValueGloves >0){
+     document.getElementById("howManyItemsGloves").value = currentValueGloves - 1;
+ }
+}
+
+let currentValueJacket;
+function increaseJacket(){
+
+     currentValueJacket = parseInt(document.getElementById("howManyItemsJacket").value);
+
+     if(currentValueJacket >=0){
+     document.getElementById("howManyItemsJacket").value = currentValueJacket + 1;
+ }
+}
+
+function decreaseJacket(){
+
+     currentValueJacket = parseInt(document.getElementById("howManyItemsJacket").value);
+
+     if(currentValueJacket >0){
+     document.getElementById("howManyItemsJacket").value = currentValueJacket - 1;
+ }
+}
+
+let currentValueShirt;
+function increaseShirt(){
+
+     currentValueShirt = parseInt(document.getElementById("howManyItemsShirt").value);
+
+     if(currentValueShirt >=0){
+     document.getElementById("howManyItemsShirt").value = currentValueShirt + 1;
+ }
+}
+
+function decreaseShirt(){
+
+     currentValueShirt = parseInt(document.getElementById("howManyItemsShirt").value);
+
+     if(currentValueShirt >0){
+     document.getElementById("howManyItemsShirt").value = currentValueShirt - 1;
+ }
+}
+
+let currentValueShort;
+function increaseShort(){
+
+     currentValueShort = parseInt(document.getElementById("howManyItemsShort").value);
+
+     if(currentValueShort >=0){
+     document.getElementById("howManyItemsShort").value = currentValueShort + 1;
+ }
+}
+
+function decreaseShort(){
+
+     currentValueShort = parseInt(document.getElementById("howManyItemsShort").value);
+
+     if(currentValueShort >0){
+     document.getElementById("howManyItemsShort").value = currentValueShort - 1;
+ }
+}
+
+let currentValueSportBag;
+function increaseSportBag(){
+
+     currentValueSportBag = parseInt(document.getElementById("howManyItemsSportBag").value);
+
+     if(currentValueSportBag >= 0){
+     document.getElementById("howManyItemsSportBag").value = currentValueSportBag + 1;
+ }
+}
+
+function decreaseSportBag(){
+
+     currentValueSportBag = parseInt(document.getElementById("howManyItemsSportBag").value);
+
+     if(currentValueSportBag > 0){
+     document.getElementById("howManyItemsSportBag").value = currentValueSportBag - 1;
+ }
+}
+
+let currentValueHoodie;
+function increaseHoodie(){
+
+     currentValueHoodie = parseInt(document.getElementById("howManyItemsHoodie").value);
+
+     if(currentValueHoodie >= 0){
+     document.getElementById("howManyItemsHoodie").value = currentValueHoodie + 1;
+ }
+}
+
+function decreaseHoodie(){
+
+     currentValueHoodie = parseInt(document.getElementById("howManyItemsHoodie").value);
+
+     if(currentValueHoodie > 0){
+     document.getElementById("howManyItemsHoodie").value = currentValueHoodie - 1;
+}
+}
